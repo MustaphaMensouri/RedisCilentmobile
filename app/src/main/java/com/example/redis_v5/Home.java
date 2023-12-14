78d80e5e43853918ce0c025f3750111ca701f876
Mustapha mensouri <mestaphamansouri@gmail.com>
2023-12-15 00:50:00 +0100

    this the frontend for the show key interface

--- app/src/main/java/com/example/redis_v5/Home.java
+++ app/src/main/java/com/example/redis_v5/Home.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -83,6 +84,17 @@ public class Home extends AppCompatActivity implements Dailog_keys.DialogListene
         l = findViewById(R.id.list_view);
         relativeLayout = findViewById(R.id.sheet);
 
+        l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Handle item click
+                String selectedItem = (String) parent.getItemAtPosition(position);
+                Toast.makeText(Home.this, "Clicked on: " + selectedItem, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Home.this, ShowKey.class);
+                intent.putExtra("key_name", selectedItem);
+                startActivity(intent);
+            }
+        });
 
 
 //        this if we click the new key dialog should pop up
--- app/src/main/java/com/example/redis_v5/ShowKey.java
+++ app/src/main/java/com/example/redis_v5/ShowKey.java
@@ -2,13 +2,20 @@ package com.example.redis_v5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ShowKey extends AppCompatActivity {
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_key);
-    }
+        Intent intent = getIntent();
+        if (intent != null) {
+            String receivedData = intent.getStringExtra("key_name");
+
+        }
+        }
 }
